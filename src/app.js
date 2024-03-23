92f5257117a4a24d7f5b3ea4ce7fb0a90251abb6
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-03-23 03:32:48 +0000

    Generador de Cartas Aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardPocker = () => {
+    let randomNumber = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let randomIcon = ["♦", "♥", "♠", "♣"];
+    let randomNumberShow = Math.floor(Math.random() * randomNumber.length);
+    let randomIconShow = Math.floor(Math.random() * randomIcon.length);
+    return {
+      randomNumber: randomNumber[randomNumberShow],
+      randomIcon: randomIcon[randomIconShow]
+    };
+  };
+
+  let iconcard = cardPocker().randomIcon;
+
+  document.querySelector(".div-center").innerText = cardPocker().randomNumber;
+
+  document.querySelectorAll(".div-right, .div-left").forEach(value => {
+    value.innerText = iconcard;
+  });
 };
