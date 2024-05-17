87f05c098774a83ae62c4f10ba313f7dc078b682
diazmoraj <103903062+diazmoraj@users.noreply.github.com>
2024-05-17 05:44:02 +0000

    Random Card Generator

--- src/app.js
+++ src/app.js
@@ -5,38 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  let cardPocker = () => {
-    let randomNumber = [
-      "A",
-      "2",
-      "3",
-      "4",
-      "5",
-      "6",
-      "7",
-      "8",
-      "9",
-      "10",
-      "J",
-      "Q",
-      "K"
-    ];
-    let randomIcon = ["♦", "♥", "♠", "♣"];
-    let randomNumberShow = Math.floor(Math.random() * randomNumber.length);
-    let randomIconShow = Math.floor(Math.random() * randomIcon.length);
-    return {
-      randomNumber: randomNumber[randomNumberShow],
-      randomIcon: randomIcon[randomIconShow]
-    };
-  };
+  document.querySelector(".poker-card").innerHTML = cardPocker();
+  document.querySelector(".poker-card").classList.add(cardIcon());
+};
 
-  let iconcard = cardPocker().randomIcon;
+let cardPocker = () => {
+  let randomNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomNumberShow = Math.floor(Math.random() * randomNumber.length);
+  return randomNumber[randomNumberShow];
+};
 
-  document.querySelector(".div-center").innerText = cardPocker().randomNumber;
-
-  document.querySelectorAll(".div-right, .div-left").forEach(value => {
-    value.innerText = iconcard;
-  });
+let cardIcon = () => {
+  let randomIcon = ["diamond", "spade", "heart", "club"];
+  let randomIconShow = Math.floor(Math.random() * randomIcon.length);
+  return randomIcon[randomIconShow];
 };
